be13133954f7c40d282ba14b6a78f064cecdb353
Karina Cárdenas <Kalejac@gmail.com>
2022-10-15 14:24:19 -0300

    Update correo.js

--- correo.js
+++ correo.js
@@ -3,15 +3,15 @@ const nodemailer = require("nodemailer");
 let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: "kcard.10013@gmail.com",
-        pass: "tfxrelriqwhgwfqh",
+        user: "-",
+        pass: "-",
     }
 })
 
 const send = async (ganador, correos, premio) => {
     let mailOptions = {
-        from: "kcard.10013@gmail.com",
-        to: ["kcard.10013@gmail.com"].concat(correos),
+        from: "-",
+        to: ["-"].concat(correos),
         subject: `${ganador.nombre} ha ganado`,
         html: `<h1>Anuncio: El ganador de ¿Quien Ganará? es ${ganador.nombre} y ha ganado: ${premio}. <br/>Gracias por Participar en el concurso.</h1>`
     }
